62179b5e3bfb72054309bf2cfb1f2819198264d1
Anushka Rodrigo <anushka.rodrigo14@gmail.com>
2026-09-21 08:18:33 +0530

    routing and navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main><h1>Hello</h1></main>
+    <main>
+      <h1>Hello</h1>
+      <Link href="/users">User</Link> {/*client side navigation*/}
+      {/*Not best way to put hyper link because rsrc downloaded again and again per page load*/}
+      {/*<a href="/users">User</a>*/}
+    </main>
   )
 }
