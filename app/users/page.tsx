53ef0bc2d8e201d14123f058c318d819e3e04703
Anushka Rodrigo <anushka.rodrigo14@gmail.com>
2026-09-21 09:29:32 +0530

    caching

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -7,7 +7,11 @@ interface User {
 }
 
 const UsersPage = async () => {
-    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const res = await fetch(
+        'https://jsonplaceholder.typicode.com/users', 
+        //{ cache: 'no-store' } to avoid caching and get fresh data on every request
+        { next: { revalidate: 10 } } //revalidate data every 10 seconds, this is caching
+    );
     const users: User[] = await res.json();
 
     return (
